eed0be8b6ddd5d49419058a202c061ddc819521f
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2021-01-06 23:38:33 -0800

    Add dotenv

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const app = require('./src/app');
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
