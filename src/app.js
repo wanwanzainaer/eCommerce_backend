ac87e30651a6c5fe68d03031ba448ac4ee3f9d14
simon <wanwanzainaer@gmail.com>
2021-01-19 15:17:21 -0800

    Add multer lib and upload path uploadroute

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const productRouter = require('./routes/productRoute');
 const userRouter = require('./routes/userRoute');
 const orderRouter = require('./routes/orderRoute');
+const uploadRouter = require('./routes/uploadRoute');
 
 const errorController = require('./controllers/errorController');
 
@@ -16,11 +18,12 @@ app.use(express.json());
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
-
+app.use('/api/upload', uploadRouter);
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use((req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
---
+++ src/routes/uploadRoute.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const uploadRouter = require('express').Router();
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+  if (extname && mimetype) return cb(null, true);
+  return cb('Images only!');
+}
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+//
+uploadRouter.route('/').post(upload.single('image'), (req, res) => {
+  res.send(`/${reqq.file.path}`);
+});
+
+module.exports = uploadRouter;
