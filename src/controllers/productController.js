eafad4cdcd7e85c9637903dd7955c3a2e51946da
simon <wanwanzainaer@gmail.com>
2021-01-18 23:54:39 -0800

    Add the product create and patch

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -19,3 +19,32 @@ exports.deleteProduct = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({});
 });
+
+exports.createProduct = asyncHandler(async (req, res, next) => {
+  // const product await Product.create({...req.body});
+  const product = await Product.create({
+    name: 'Smaple name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  });
+
+  res.status(201).json(product);
+});
+
+exports.updateProduct = asyncHandler(async (req, res, next) => {
+  // const product await Product.create({...req.body});
+  const updateProduct = await Product.findByIdAndUpdate(req.params.id, {
+    ...req.body,
+  });
+
+  if (!updateProduct)
+    return next(new HttpError('Can not find the product', 404));
+
+  res.status(201).json(updateProduct);
+});
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -2,11 +2,15 @@ const express = require('express');
 const productRouter = express.Router();
 const productController = require('../controllers/productController');
 const { admin, protect } = require('../middlewares/authMiddlewares');
-productRouter.route('/').get(productController.getAllProducts);
+productRouter
+  .route('/')
+  .get(productController.getAllProducts)
+  .post(protect, admin, productController.createProduct);
 
 productRouter
   .route('/:id')
   .get(productController.getProduct)
-  .delete(protect, admin, productController.deleteProduct);
+  .delete(protect, admin, productController.deleteProduct)
+  .patch(protect, admin, productController.updateProduct);
 
 module.exports = productRouter;
